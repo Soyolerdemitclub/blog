5455f3904ec9747e831fc7c4c8d41dc3691e1e15
KatsuraMN <katsuramanakana@gmail.com>
2016-02-20 15:37:55 +0800

    tohiruulga

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ActiveRecord::Base
+	has_attached_file :avatar, styles: { medium: "548x400>", thumb: "800x800>" }, default_url: "/images/:style/missing.png"
+    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+    extend FriendlyId
+   friendly_id :title, use: :slugged
+end
