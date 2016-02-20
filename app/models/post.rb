4eb272d1bb0c9036236357112bea2f54931f7524
katsuramn <katsuramanakana@gmail.com>
2016-02-20 15:52:41 +0800

    zagwar shinechlel

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :avatar, styles: { medium: "548x400>", thumb: "800x800>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
     extend FriendlyId
    friendly_id :title, use: :slugged
